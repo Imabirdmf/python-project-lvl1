7e401ec48b6d79eb531695150e0524f271298a7b
RedBird <vash.stampide@gmail.com>
2021-06-15 22:50:37 +0300

    all good

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,5 @@ import prompt
 
 
 def welcome_user():
-    """Welcome user message."""
     name = prompt.string('May I have you name? ')
-    print('Hello, {}!'.format(name))
+    return 'Hello, {}!'.format(name)

93314691a12a9764a17255ab11e692741f37972e
RedBird <vash.stampide@gmail.com>
2021-06-16 13:15:38 +0300

    first game working

--- brain_games/first_game.py
+++ brain_games/first_game.py
@@ -5,16 +5,18 @@ import random
 def welcome_user():
     name = prompt.string('May I have you name? ')
     print('Hello, {}!'.format(name))
+    return name
 
 
-def even_game():
-    i = 0
-    while i < 3:
+def even_game(name):
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for _ in range(1, 4):
         number = random.randint(1, 101)
-        answer = prompt.string('Answer "yes" if the number is even, otherwise answer "no". \nQuestion: {}'.format(number))
-        if (number % 2 == 0 and answer == 'yes') or (number % 2 !=0 and answer == 'no'):
+        answer = input('Question: {}\nYour answer: '.format(number))
+        if (number % 2 == 0 and answer == 'yes') or (number % 2 != 0 and answer == 'no'):
             print('Correct!')
-            i += 1
         else:
-            return "'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {}!".format(name)
-    return 'Congratulations, {}!'.format(name)
+            print("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {}!".format(name))
+            break  # Здесь потом можно возвращать значение
+    else:
+        print('Congratulations, {}!'.format(name))  # Здесь потом можно возвращать значение
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,8 +6,8 @@ from brain_games import first_game
 
 def main():
     print('Welcome to the Brain Games!')
-    first_game.welcome_user()
-    first_game.even_game()
+    name = first_game.welcome_user()
+    first_game.even_game(name)
 
 
 if __name__ == '__main__':

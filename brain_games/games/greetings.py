c166ec44af36efedb624a20165a345b3c0d1c977
RedBird <vash.stampide@gmail.com>
2021-06-30 00:07:17 +0300

    update prime game

--- brain_games/games/greetings.py
+++ brain_games/games/greetings.py
@@ -3,14 +3,15 @@ import prompt
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print('Hello, {0}!'.format(name))
     return name
 
 
 def congratulations(name):
-    print('Congratulations, {}!'.format(name))
+    print('Congratulations, {0}!'.format(name))
 
 
 def failure(answer, total, name):
-    print('{} is wrong answer ;(. Correct answer was {}.'.format(answer, total))
-    print("Let's try again, {}!".format(name))
+    print("'{0}' is wrong answer ;(.".format(answer), end=' ')
+    print("Correct answer was '{0}'.".format(total))
+    print("Let's try again, {0}!".format(name))
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from brain_games.games import greetings, prime_game
+from brain_games.games import engine, greetings, prime_game
 
 
 def main():
     print('Welcome to the Brain Games!')
     name = greetings.welcome_user()
-    prime_game.is_prime(name)
+    engine.game_circle(name, prime_game)
 
 
 if __name__ == '__main__':

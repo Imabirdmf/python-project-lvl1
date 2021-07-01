94e7dc8da03d6c55792249fac4a8b823baf30036
RedBird <vash.stampide@gmail.com>
2021-07-01 22:17:43 +0300

    fix#3

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -9,9 +9,7 @@ def calculate():
     num1 = random.randint(l_border, r_border)
     num2 = random.randint(l_border, r_border)
     question = 'Question: {0} {1}'.format(num1, num2)
-    for index in range(1, (max(num1, num2) // 2) + 1):
+    for index in range(1, (min(num1, num2)) + 1):
         if num1 % index == 0 and num2 % index == 0:
             total = index
-        if index > min(num1, num2):
-            break
     return (question, total)

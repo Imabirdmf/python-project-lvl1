bb30c349359192f308b6ab05bcb68584b3705a75
RedBird <vash.stampide@gmail.com>
2021-07-13 00:09:56 +0300

    review #3

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -8,9 +8,14 @@ STOP = 100
 
 def get_gcd(num1, num2):
     min_num = min(num1, num2)
-    for index in range(1, (min_num + 1)):
-        if num1 % index == 0 and num2 % index == 0:
-            right_answer = index
+    max_num = max(num1, num2)
+    while max_num != 0 and min_num != 0:
+        q = max_num % min_num
+        if q == 0:
+            right_answer = min_num
+            break
+        max_num = min_num
+        min_num = q
     return right_answer
 
 

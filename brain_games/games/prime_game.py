9a6949c733c0b7e4de8681030f20f93b0fa80a59
RedBird <vash.stampide@gmail.com>
2021-07-15 22:58:06 +0300

    #3

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -7,19 +7,18 @@ STOP = 100
 
 
 def is_prime(num):
-    flag = True
+    if num <= 1:
+        return False
     for index in range(2, int(num ** 0.5) + 1):
         if num % index == 0:
-            flag = False
-            break
-    return flag
+            return False
+    return True
 
 
 def get_right_answer():
     num = random.randint(START, STOP)
     question = str(num)
-    flag = is_prime(num)
-    if num > 1 and flag is True:
+    if is_prime(num):
         right_answer = 'yes'
     else:
         right_answer = 'no'

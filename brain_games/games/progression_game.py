098bf1edf9221dc32a2a49d96c602f81cd4cc5df
RedBird <vash.stampide@gmail.com>
2021-07-17 17:17:47 +0300

    #4

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -5,19 +5,21 @@ rule = 'What number is missing in the progression?'
 START = 1
 STOP = 20
 LENGTH = 10
+progression_start = random.randint(START, STOP)
+diff_start = 1
+diff_stop = 5
+diff = random.randint(diff_start, diff_stop)
+position = random.randint(0, LENGTH - 1)
 
 
 def create_progression():
-    progression_start = random.randint(START, STOP)
-    diff = random.randint(1, 5)
     progression_stop = progression_start + LENGTH * diff
     return list(range(progression_start, progression_stop, diff))
 
 
 def get_right_answer():
-    position = random.randint(0, LENGTH - 1)
     progression = create_progression()
     right_answer = progression[position]
     progression[position] = '..'
     progression = ' '.join(str(number) for number in progression)
-    return ('{0}'.format(progression), right_answer)
+    return (progression, right_answer)
